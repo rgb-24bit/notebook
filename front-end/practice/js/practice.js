37d2dce629286305b324c892b13b04c1110e62c1
rgb-24bit <rgb-24bit@foxmail.com>
2018-03-28 21:42:21 +0800

    front-end: 添加 js 笔记

---
+++ front-end/practice/js/practice.js
@@ -0,0 +1,15 @@
+var inner = "From JavaScript";
+var write = 2.333333333333333;
+
+
+// 定义函数
+function myfunc() {
+    document.getElementById("test").innerHTML=inner;
+}
+
+function myfunc2() {
+    document.write(write);
+}
+
+// 调用函数
+myfunc();
