06a286e61606154a2b868639fbafcd7c5cd649b1
rgb-24bit <rgb-24bit@foxmail.com>
2018-04-02 19:12:05 +0800

    note: 修改 generate.py

--- generate.py
+++ generate.py
@@ -18,59 +18,22 @@ import os
 import glob
 
 
-class Storage(dict):
-    def __getattr__(self, key):
-        try:
-            return self[key]
-        except KeyError as k:
-            raise AttributeError(k)
-
-    def __setattr__(self, key, value):
-        self[key] = value
-
-    def __delattr__(self, key):
-        try:
-            del self[key]
-        except KeyError as k:
-            raise AttributeError(k)
-
-    def __repr__(self):
-        return '<Storage ' + dict.__repr__(self) + '>'
+def get_glob_pattern(file_name='./config'):
+    with io.open(file_name, 'r', encoding='utf-8') as fp:
+        for line in fp:
+            if line.strip() and line[0] != '#':
+                yield line.strip()
 
 
-def glob_dir(dir_name, glob_pattern=list()):
-    """ 获取指定目录下符合 glob pattern 的 sub_dir, sub_file."""
-    glob_res = Storage(sub_dir=list(), sub_file=list())
-
-    for pattern in glob_pattern:
-        for match in glob.glob(os.path.join(dir_name, pattern)):
-            if os.path.isdir(match):
-                glob_res.sub_dir.append(match)
-            else:
-                glob_res.sub_file.append(match)
-
-    return glob_res
-
-
-def get_glob_pattern(dir_name):
-    """ 获取该目录的 glob 匹配模式."""
-    glob_pattern = list()
-
-    file_name = os.path.join(dir_name, '.generate')
-    if os.path.isfile(file_name):
-        with io.open(file_name, 'r', encoding='utf-8') as fp:
-            for line in fp:
-                if not line[0] == '#':
-                    glob_pattern.append(line.strip())
-    return glob_pattern
+def get_req_file(pattern):
+    for f in glob.glob(pattern):
+        yield f
 
 
 def get_file_title(file_name):
-    """ 获取文件标题."""
     with io.open(file_name, 'r', encoding='utf-8') as fp:
-        pattern = r'#\+TITLE:.+'
+        match = re.search(r'#\+TITLE:.+', fp.read())
 
-        match = re.search(pattern, fp.read())
         if match:
             title = match.group(0).split(':')[-1]
             return title.strip()
@@ -78,39 +41,19 @@ def get_file_title(file_name):
         return os.path.split(file_name)[-1]
 
 
-def generate_content(root_dir, root_glob_pattern, stream, digree=0):
-    """ 生成 README.org 的内容"""
-    kind_format = '- *{name}*\n'
-    link_format = '- [[file:{path}][{name}]]\n'
+def generate(file_name='README.org'):
+    list_format = '- {name} -- [[file:{path}][{title}]] ::\n'
+    with io.open(file_name, 'w', encoding='utf-8') as fp:
+        fp.write('#+TITLE: Note\n\n')
 
-    glob_pattern = get_glob_pattern(root_dir)
-    if not glob_pattern:
-        glob_pattern = root_glob_pattern
+        sep = '\\' if os.name == 'nt' else '/'
 
-    glob_res = glob_dir(root_dir, glob_pattern)
-    for sub_dir in glob_res.sub_dir:
-        # TODO 去除重复 glob 匹配
-        if glob_dir(sub_dir, glob_pattern).sub_file:
-            kind = digree * '  ' + kind_format.format(
-                name = os.path.basename(sub_dir[0:-1])
-            )
-            stream.write(kind)
-
-            generate_content(sub_dir, glob_pattern, stream, digree + 1)
-
-    for sub_file in glob_res.sub_file:
-        link = digree * '  ' + link_format.format(
-            path = sub_file,
-            name = get_file_title(sub_file)
-        )
-        stream.write(link)
-
-def generate_file(root_dir, file_name):
-    """ 生成 README.org 文件"""
-    with io.open(file_name, 'w', encoding='utf-8') as stream:
-        stream.write('#+TITLE: Table of Contents\n\n')
-        generate_content(root_dir, list(), stream, 0)
+        for pt in get_glob_pattern():
+            for path in get_req_file(pt):
+                name = os.path.normpath(path).split(sep)[0]
+                title = get_file_title(path)
+                fp.write(list_format.format(name=name, path=path, title=title))
 
 
 if __name__ == '__main__':
-    generate_file('.', 'README.org')
+    generate()
