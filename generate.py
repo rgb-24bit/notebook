94ab3282b8b7a4fff10e98bd382ac4d4002867db
rgb-24bit <rgb-24bit@foxmail.com>
2018-11-03 16:01:41 +0800

    note: 更新生成脚本

--- generate.py
+++ generate.py
@@ -8,51 +8,55 @@ Generate README.org
 2. Depth does not exceed 1.
 """
 
-import json
+import pathlib
 import re
 
-from pathlib import Path
+
+IGNORE_DIR = ('.git', 'inbox', 'math', '_style', '__pycache__')
 
 
-IGNORE = ('.git', 'inbox', 'math', '_style', '__pycache__')
+class DetailsWriter(object):
+    def __init__(self, fileobj, summary):
+        self._fileobj = fileobj
+        self._summary = summary
 
+    def __enter__(self):
+        line = '#+HTML: <details><summary><b><i>%s</i></b></summary>\n' % self._summary
+        self._fileobj.write(line)
+        return self
 
-class OrgWrite(object):
-    @staticmethod
-    def write_list(stream, name, deep=0):
-        stream.write('  ' * deep + '- %s ::\n' % name)
+    def __exit__(self, type_, value, trace):
+        self._fileobj.write('#+HTML: </details>\n')
 
-    @staticmethod
-    def write_link(stream, name, path, deep=0):
-        stream.write('  ' * deep + '- [[file:%s][%s]]\n' % (path, name))
-
-    @staticmethod
-    def write_title(stream, name):
-        stream.write('* ' + name + '\n')
+    def write(self, path, name):
+        self._fileobj.write('- [[file:%s][%s]]\n' % (path, name))
 
 
 def get_note_dir():
-    for fn in Path().iterdir():
-        if fn.name in IGNORE or not fn.is_dir():
+    """Get the directory containing the notes."""
+    for fn in pathlib.Path().iterdir():
+        if fn.name in IGNORE_DIR or not fn.is_dir():
             continue
         yield fn
 
+
 def get_note_name(note):
+    """Get the name of the note."""
     with open(note, encoding='utf-8') as fp:
         match = re.search(r'#\+TITLE:\s*(.+)', fp.read())
         if match:
             return match.group(1)
-    return Path(note).stem
+    return pathlib.Path(note).stem
 
 
-def generate_readme(filename):
-    with open(filename, 'w+', encoding='utf-8') as stream:
+def generate(fn):
+    """Generate a README document."""
+    with open(fn, 'w', encoding='utf-8') as stream:
         for note_dir in get_note_dir():
-            OrgWrite.write_title(stream, note_dir.name)
-
-            for note in note_dir.glob('**/*.org'):
-                OrgWrite.write_link(stream, get_note_name(note), note.as_posix(), 1)
+            with DetailsWriter(stream, note_dir.name) as writer:
+                for note in note_dir.glob('**/*.org'):
+                    writer.write(note.as_posix(), get_note_name(note))
 
 
 if __name__ == '__main__':
-    generate_readme('README.org')
+    generate('README.org')
